0128e508f9120b4a5f627d021972d8cd8f19a2b4
Olivier G <zotri@hotmail.com>
2024-10-04 16:40:05 +0200

    removed window navigation mapping

--- lua/ocgg/maps.lua
+++ lua/ocgg/maps.lua
@@ -15,10 +15,10 @@ map("n", "<leader>v", "<CMD>vsplit<CR>", "Vertical Split")
 map("n", "<leader>x", "<CMD>split<CR>", "Horizontal Split")
 
 -- Window Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
+-- map("n", "<C-h>", "<C-w>h")
+-- map("n", "<C-l>", "<C-w>l")
+-- map("n", "<C-k>", "<C-w>k")
+-- map("n", "<C-j>", "<C-w>j")
 
 -- Resize Windows
 map("n", "<C-Left>", "<C-w><")
