92b58b5e2a533f1ebc5192060786eb1b11888b73
Olivier G <zotri@hotmail.com>
2024-10-04 13:47:04 +0200

    tabstop & shiftwidth 4 + guidecol 80

--- lua/ocgg/settings.lua
+++ lua/ocgg/settings.lua
@@ -1,7 +1,7 @@
 local global = vim.g
 local o = vim.opt
 
-global.mapleader = ' '
+global.mapleader = " "
 
 -- required for nvim-tree
 vim.g.loaded_netrw = 1
@@ -9,23 +9,23 @@ vim.g.loaded_netrwPlugin = 1
 
 o.number = true
 o.relativenumber = true
-o.clipboard = 'unnamedplus'
-o.syntax = 'on'
+o.clipboard = "unnamedplus"
+o.syntax = "on"
 -- o.autoindent = true
 -- o.cursorline = true
 o.expandtab = true
-o.shiftwidth = 2
-o.tabstop = 2
+o.shiftwidth = 4
+o.tabstop = 4
 -- o.encoding = "UTF-8"
 -- o.ruler = true
-o.mouse = 'a'
+o.mouse = "a"
 o.title = true
 -- o.hidden = true
 o.ttimeoutlen = 0
 -- o.wildmenu = true
 -- o.showcmd = true
 o.showmatch = true
-o.inccommand = 'split'
+o.inccommand = "split"
 o.splitright = true
 o.splitbelow = true
 o.termguicolors = true
@@ -35,8 +35,10 @@ o.breakindent = true
 o.undofile = true
 o.ignorecase = true
 o.smartcase = true
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 -- Decrease update time
 o.updatetime = 250
 o.timeoutlen = 300
 -- o.completeopt = 'menuone,noselect'
+-- vertical line
+o.colorcolumn = "80"
