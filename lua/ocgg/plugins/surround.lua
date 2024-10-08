26abf1a8a819aa59ff208ee7a74c4633c350b65d
Olivier G <zotri@hotmail.com>
2024-10-08 23:39:15 +0200

    surround config

--- lua/ocgg/plugins/surround.lua
+++ lua/ocgg/plugins/surround.lua
@@ -4,7 +4,26 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("nvim-surround").setup({
-			-- Configuration here, or leave empty to use defaults
+			-- Defaults to remember:
+
+			-- keymaps = {
+			--     insert = "<C-g>s",
+			--     insert_line = "<C-g>S",
+			--     normal = "ys",
+			--     normal_cur = "yss",
+			--     normal_line = "yS",
+			--     normal_cur_line = "ySS",
+			--     visual = "S",
+			--     visual_line = "gS",
+			--     delete = "ds",
+			--     change = "cs",
+			--     change_line = "cS",
+			-- },
+			-- aliases = {
+			--     ["q"] = { '"', "'", "`" },
+			--     ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+			-- },
+			move_cursor = "sticky",
 		})
 	end,
 }
