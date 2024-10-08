1b5aae4d1b24a33793f99dcd12b8037c8590d258
Olivier G <zotri@hotmail.com>
2024-10-08 23:13:23 +0200

    add highlight on yank

--- lua/ocgg/autocommands.lua
+++ lua/ocgg/autocommands.lua
@@ -10,3 +10,13 @@ vim.api.nvim_create_autocmd("CursorMoved", {
 		end
 	end,
 })
+
+-- Highlight on yank (from kickstart)
+-- See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+	pattern = "*",
+})
